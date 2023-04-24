7b78075e232af0aef983391b3d8dcfe5fd2a507a
yxj0312 <yanxiajun@hotmail.com>
2023-04-24 22:39:20 +0200

    add content api

--- components/BlogPostList.vue
+++ components/BlogPostList.vue
@@ -1,10 +1,16 @@
-<script setup></script>
+<script setup>
+const { data: blogPostList } = useAsyncData('blogPostList', ()=>{
+  return queryContent('/blog').find()
+})
+
+
+</script>
 
 <template>
   <div class="container">
     <section class="articles">
       <div class="column is-8 is-offset-2">
-        <div class="card article">
+        <div v-for="blogPost in blogPostList" class="card article">
           <NuxtLink to="/blog/my-second-blog-post">
             <section class="blog-post-card card article">
               <div class="media">
@@ -23,25 +29,7 @@
             </section>
           </NuxtLink>
         </div>
-        <div class="card article">
-          <NuxtLink to="/blog/my-first-blog-post">
-            <section class="blog-post-card card article">
-              <div class="media">
-                <div class="media-content has-text-centered">
-                  <h3 class="title article-title has-text-weight-bold">
-                    Hard-Coded Blog Post #1
-                  </h3>
-                  <BlogPostMeta author="@luisa" date="2022-05-01" />
-                </div>
-              </div>
-              <div class="card-content">
-                <div class="content article-body is-size-5">
-                  Here is the hard-coded description for blog #1.
-                </div>
-              </div>
-            </section>
-          </NuxtLink>
-        </div>
+        
       </div>
     </section>
   </div>
